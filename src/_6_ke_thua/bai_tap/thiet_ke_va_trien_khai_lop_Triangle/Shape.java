fb7b23cc1dddc5f8b6e145ce61647dbff96ada06
nguyentrihuyen1810 <93263209+nguyentrihuyen1810@users.noreply.github.com>
2022-01-18 11:53:47 +0700

    push bài tập

--- src/_6_ke_thua/bai_tap/thiet_ke_va_trien_khai_lop_Triangle/Shape.java
+++ src/_6_ke_thua/bai_tap/thiet_ke_va_trien_khai_lop_Triangle/Shape.java
@@ -36,12 +36,4 @@ public class Shape {
                 + " and"
                 + (isFilled() ? " filled" : " not filled");
     }
-
-    public static void main(String[] args) {
-        Shape shape = new Shape();
-        System.out.println(shape);
-
-        shape = new Shape("red", false);
-        System.out.println(shape);
-    }
 }
